334a2e5f278b1c5dd6e4f285f22efc62fc5af9da
Christopher <chrissi.peters98@web.de>
2022-12-02 06:22:46 +0100

    added text and button scalings

--- STDgame/Assets/Quiz Folder/QuizScript.cs
+++ STDgame/Assets/Quiz Folder/QuizScript.cs
@@ -55,10 +55,10 @@ void changeTexts(int question)
     }
     void addMockQuestions()
     {
-  Question question1 = new Question("Quel symptôme peut être provoqué par le papillomavirus ?", new string[] { "Une prise de poids", "La myopie", "Des verrues génitales", "Des plaques rouges sur la peau" }, 3);
-     Question question2 = new Question("Comment améliorer l’efficacité d’un préservatif?", new string[] { "En choisir un adapté à sa taille", "Mettre 2 préservatifs", "Le pré-mâcher ", "Éviter les préservatifs normés NF" }, 3);
-        Question question3 = new Question("De quoi est constitué l'intérieur du pénis ?", new string[] { "De muscles", "De corps caverneux", "De sperme", "De bois" }, 2);
-        Question question4 = new Question("The penis is in the …", new string[] { "kitchen", "stadium", "pie", "condom" }, 4);
+  Question question1 = new Question("Quel symptôme peut être provoqué par le papillomavirus ?", new string[] { "Une prise de poids", "La myopie", "Des verrues génitales", "Des plaques rouges sur la peau" }, 3, "Les condylomes externes, aussi appelés verrues génitales ou « crêtes de coq » sont des excroissances indolores pouvant apparaître sur la verge, la vulve, autour du vagin ou de l’anus. Parfois, ils entraînent des démangeaisons, mais la plupart du temps ils restent silencieux. On les fait enlever car ce n’est pas très joli, mais en réalité ils ne sont pas dangereux. Ils peuvent parfois revenir après le traitement.");
+     Question question2 = new Question("Comment améliorer l’efficacité d’un préservatif?", new string[] { "En choisir un adapté à sa taille", "Mettre 2 préservatifs", "Le pré-mâcher ", "Éviter les préservatifs normés NF" }, 3, "Un préservatif adapté à sa taille évitera qu’il glisse ou marque le pénis. Contrairement à ce qu’on pourrait penser, passer 2 préservatifs l’un sur l’autre augmente le risque qu’il de casse. La norme NF sur un préservatif renseigne un gage de qualité !");
+        Question question3 = new Question("De quoi est constitué l'intérieur du pénis ?", new string[] { "De muscles", "De corps caverneux", "De sperme", "De bois" }, 2, "Le pénis est constitué de corps caverneux. Lors d’une érection, ces corps se remplissent de sang et font gonfler le pénis. Une érection peut-être provoquée lorsque l’individu est excité.");
+        Question question4 = new Question("The penis is in the …", new string[] { "kitchen", "stadium", "pie", "condom" }, 4 ,"Sortez couverts !!!");
         questions = new List<Question>();
         questions.Add(question1);
         questions.Add(question2); questions.Add(question3); questions.Add(question4);
